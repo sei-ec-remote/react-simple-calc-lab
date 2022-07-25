b6c825768fe67a23ceb282a9cac965c862cd63e4
Alys <alice.g.cooper@gmail.com>
2022-07-25 14:08:49 -0400

    Set up adding function

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
 
 class Calculator extends Component {
+    state = {
+        num1: '',
+        num2: '',
+        sum: 0,
+    }
+
+    //want to make a method that will intake user input into an array and reduce the array (add them)
+    setNum = (e, num) => {
+        this.setState({ [num]: e.target.value});
+    }
+
+    setSum = () => {
+        const newSum = parseInt(this.state.num1) + parseInt(this.state.num2)
+        // want to add both num1 and num2 together
+        this.setState(() => {
+            return {
+                sum: newSum,
+            }
+        })
+    }
     render() {
         return (
             <div className="container">
                 <h1>Add with React!</h1>
 
                 <div className="add">
-                    <input type="text" />
+                    <input type="number"
+                        name="num1"
+                        placeholder="Enter your first number"
+                        value={this.state.num1}
+                        onChange={ (e) => this.setNum(e, 'num1') }
+                    />
                     <span>+</span>
-                    <input type="text" />
-                    <button>=</button>
-                    <h3>Addition results go here!</h3>
+                    <input type="number"
+                        name="num1"
+                        placeholder="Enter your first number"
+                        value={this.state.num2}
+                        onChange={ (e) => this.setNum(e, 'num2') }
+                    />
+                    <button 
+                        onClick={this.setSum}
+                    >
+                        =
+                    </button>
+                    <h3>Addition results go here: {this.state.sum}</h3>
                 </div>
             </div>
         );
