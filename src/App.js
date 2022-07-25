ce8eafd70153de2d4c0505e189ba705160152d88
eshalali <eshal.a.ali@gmail.com>
2022-07-25 13:19:28 -0500

    Completed lab

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './Calculator'
+
+function App() {
+  return (
+    <div className="App">
+      <Calculator 
+      
+      />
+    </div>
+  );
+}
+
+export default App;
